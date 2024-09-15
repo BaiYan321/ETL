d91bdd9f5b6443903bbe61bb101ad217d7fb3819
BaiYan321 <ryan.baiyan@gmail.com>
2024-09-15 00:28:02 -0700

    build api with dockerfile and working on the docker-compose for dag

--- nyt_api.py
+++ nyt_api.py
@@ -34,20 +34,19 @@ url = create_nyt_url('nvidia',10)
 json_response = connect_to_nyt_endpoint(url)
 
 nyt_data = json.dumps(json_response, indent=4, sort_keys=True)
-nyt_data = json.loads(nyt_data)
-print(len(nyt_data['response']['docs']))
+print(nyt_data)
+#nyt_data = json.loads(nyt_data)
+#print(len(nyt_data['response']['docs']))
 
-temp_abstract=[]
-temp_pub_date=[]
-temp_document_type=[]
-for _ in range(len(nyt_data['response']['docs'])):
-    temp_abstract.append(nyt_data['response']['docs'][_]['abstract'])
-    temp_pub_date.append(nyt_data['response']['docs'][_]['pub_date'])
-    temp_document_type.append(nyt_data['response']['docs'][_]['document_type'])
+#temp_abstract=[]
+#temp_pub_date=[]
+#temp_document_type=[]
+#for _ in range(len(nyt_data['response']['docs'])):
+#    temp_abstract.append(nyt_data['response']['docs'][_]['abstract'])
+#    temp_pub_date.append(nyt_data['response']['docs'][_]['pub_date'])
+#    temp_document_type.append(nyt_data['response']['docs'][_]['document_type'])
 
-dict = {'abstract': temp_abstract, 'pub_date': temp_pub_date, 'document_type':temp_document_type }
 
-df = pd.DataFrame(dict)
 
 
     
